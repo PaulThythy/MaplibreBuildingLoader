f22f3ed91f73a0073b799c0d46e05c3fa31751ad
Paul <paul.thi@hotmail.fr>
2023-12-08 13:12:53 +0100

    separate class

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three';
 
+import { TransformControls } from './three.js/examples/jsm/controls/TransformControls.js'
 import { IFCLoader } from 'web-ifc-three';
 import { IFCSPACE } from 'web-ifc';
+
+import { ifcCustomLayer } from './ifcCustomLayer.js';
 
 var mapOrigin = { LngLat: [5.0801, 47.3134], altitude: 0, rotation: new THREE.Vector3(Math.PI / 2, 0, 0) };
 var mapMercatorCoordinates = maplibregl.MercatorCoordinate.fromLngLat([mapOrigin.LngLat[0], mapOrigin.LngLat[1]], mapOrigin.altitude);
@@ -55,93 +58,8 @@ await ifcLoader.ifcManager.applyWebIfcConfig({
   USE_FAST_BOOLS: true
 });
 
-class CustomLayer {
-  type = 'custom';
-  renderingMode = '3d';
-
-  constructor(id) {
-    this.id = id;
-  }
-
-  async onAdd(map, gl) {
-    this.camera = new THREE.PerspectiveCamera();
-    this.scene = new THREE.Scene();
-
-    const directionalLight = new THREE.DirectionalLight(0x404040);
-    const directionalLight2 = new THREE.DirectionalLight(0x404040);
-    const ambientLight = new THREE.AmbientLight(0x404040, 100);
-
-    directionalLight.position.set(0, -70, 100).normalize();
-    directionalLight2.position.set(0, 70, 100).normalize();
-    this.scene.add(directionalLight, directionalLight2, ambientLight);
-
-    ifcLoader.load('testIFCFiles/rac_advanced_sample_project.ifc', (ifcModel) => {
-      const mesh = new THREE.Mesh(ifcModel.geometry, new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
-      this.scene.add(ifcModel.mesh);
-    });
-
-    this.map = map;
-
-    this.renderer = new THREE.WebGLRenderer({ canvas: map.getCanvas(), context: gl });
-    this.renderer.autoClear = false;
-
-    this.raycaster = new THREE.Raycaster();
-  }
-
-  render(gl, matrix) {
-    const rotationX = new THREE.Matrix4().makeRotationAxis(
-      new THREE.Vector3(1, 0, 0), mapOriginTransform.rotateX);
-    const rotationY = new THREE.Matrix4().makeRotationAxis(
-      new THREE.Vector3(0, 1, 0), mapOriginTransform.rotateY);
-    const rotationZ = new THREE.Matrix4().makeRotationAxis(
-      new THREE.Vector3(0, 0, 1), mapOriginTransform.rotateZ);
-
-    const m = new THREE.Matrix4().fromArray(matrix);
-    const l = new THREE.Matrix4()
-      .makeTranslation(
-        mapOriginTransform.translateX,
-        mapOriginTransform.translateY,
-        mapOriginTransform.translateZ
-      )
-      .scale(
-        new THREE.Vector3(
-          mapOriginTransform.scale,
-          -mapOriginTransform.scale,
-          mapOriginTransform.scale)
-      )
-      .multiply(rotationX)
-      .multiply(rotationY)
-      .multiply(rotationZ);
-
-    this.camera.projectionMatrix = m.multiply(l);
-    this.renderer.resetState();
-    this.renderer.render(this.scene, this.camera);
-    this.map.triggerRepaint();
-  }
-
-  raycast(point, isClick) {
-    var mouse = new THREE.Vector2();
-    mouse.x = ( point.x / this.map.transform.width ) * 2 - 1;
-    mouse.y = 1 - ( point.y / this.map.transform.height ) * 2;
-
-    const camInverseProjection = new THREE.Matrix4().invert(this.camera.projectionMatrix);
-    const cameraPosition = new THREE.Vector3().applyMatrix4(camInverseProjection);
-    const mousePosition = new THREE.Vector3(mouse.x, mouse.y, 1).applyMatrix4(camInverseProjection);
-    const viewDirection = mousePosition.clone().sub(cameraPosition).normalize(); 
-
-    this.raycaster.set(cameraPosition, viewDirection);
-
-    var intersects = this.raycaster.intersectObjects(this.scene.children, true);
-    console.log(intersects.length);
-  }
-}
-
-let customL = new CustomLayer('custom');
+let customL = new ifcCustomLayer('custom', './testIFCFiles/01.ifc', ifcLoader, mapOriginTransform, map);
 
 map.on('load', () => {
   map.addLayer(customL);
-});
-
-map.on('click', e => {
-  customL.raycast(e.point, true);
 });
---
+++ ifcCustomLayer.js
@@ -0,0 +1,86 @@
+import * as THREE from 'three';
+
+export class ifcCustomLayer {
+    type = 'custom';
+    renderingMode = '3d';
+
+    constructor(_id, _url, _loader, _mapOriginTransform, _map) {
+        this.id = _id;
+        this.loader = _loader;
+        this.url = _url;
+        this.mapOriginTransform = _mapOriginTransform;
+        this.map = _map;
+    }
+
+    async onAdd(map, gl) {
+        this.camera = new THREE.PerspectiveCamera();
+        this.scene = new THREE.Scene();
+
+        const directionalLight = new THREE.DirectionalLight(0x404040);
+        const directionalLight2 = new THREE.DirectionalLight(0x404040);
+        const ambientLight = new THREE.AmbientLight(0x404040, 100);
+
+        directionalLight.position.set(0, -70, 100).normalize();
+        directionalLight2.position.set(0, 70, 100).normalize();
+        this.scene.add(directionalLight, directionalLight2, ambientLight);
+
+        this.loader.load(this.url, (ifcModel) => {
+            this.scene.add(ifcModel.mesh);
+        });
+
+        /*this.raycaster = new THREE.Raycaster();
+        this.mouse = new THREE.Vector2();
+        map.getCanvas().addEventListener('mousedown', this.onMouseDown.bind(this), false);*/
+
+        this.map = map;
+
+        this.renderer = new THREE.WebGLRenderer({ canvas: this.map.getCanvas(), context: gl });
+        this.renderer.autoClear = false;
+    }
+
+    render(gl, matrix) {
+        const rotationX = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(1, 0, 0), this.mapOriginTransform.rotateX);
+        const rotationY = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(0, 1, 0), this.mapOriginTransform.rotateY);
+        const rotationZ = new THREE.Matrix4().makeRotationAxis(
+            new THREE.Vector3(0, 0, 1), this.mapOriginTransform.rotateZ);
+
+        const m = new THREE.Matrix4().fromArray(matrix);
+        const l = new THREE.Matrix4()
+            .makeTranslation(
+                this.mapOriginTransform.translateX,
+                this.mapOriginTransform.translateY,
+                this.mapOriginTransform.translateZ
+            )
+            .scale(
+                new THREE.Vector3(
+                    this.mapOriginTransform.scale,
+                    -this.mapOriginTransform.scale,
+                    this.mapOriginTransform.scale)
+            )
+            .multiply(rotationX)
+            .multiply(rotationY)
+            .multiply(rotationZ);
+
+        this.camera.projectionMatrix = m.multiply(l);
+        this.renderer.resetState();
+        this.renderer.render(this.scene, this.camera);
+        this.map.triggerRepaint();
+    }
+
+    /* onMouseDown(event) {
+      this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      this.mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+  
+      const camInverseProjection = new THREE.Matrix4().invert(this.camera.matrixWorldInverse);
+      const cameraPosition = new THREE.Vector3().applyMatrix4(camInverseProjection);
+      const mousePosition = new THREE.Vector3(this.mouse.x, this.mouse.y, 1).applyMatrix4(camInverseProjection);
+      const viewDirection = mousePosition.clone().sub(cameraPosition).normalize(); 
+  
+      this.raycaster.set(cameraPosition, viewDirection);
+  
+      var intersects = this.raycaster.intersectObjects(this.scene.children, true);
+      console.log(intersects);
+    } */
+}
